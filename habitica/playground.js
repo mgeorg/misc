7d28c9c1ccc5f70e4aceb7ab0ae802faa37eb172
Manfred Georg <mgeorg@google.com>
2022-09-05 16:16:28 -0700

    Updated comments

--- habitica/playground.js
+++ habitica/playground.js
@@ -24,18 +24,22 @@
 const USER_ID = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
 // This is a password, don't share it with anyone.
 const API_TOKEN = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
-// This needs to be a publicly accessible URL for this script.
-// To get it, click on deploy, click on the gear (settings) icon,
-// click on web app, click on permissions and select (anyone),
-// click on deploy.  You'll have to authorize the script to act
-// with your account (yes, this should feel scary), there are two
-// permissions the script needs, to run whon you're not present.
-// (so it can respond to requests from Habitica through the webhooks
-// interface, and run time delayed triggers to wait for things
-// to happen), and to contact external websites.  You can check
+// This needs to be a publicly accessible URL for this script.  To get
+// it, click on deploy, click on the gear (settings) icon, click on
+// web app, click on permissions and select (anyone), click on deploy.
+// Authorize the Web App for your account.  Copy the Web App URL into
+// the field below where it says replace_me (make sure to keep single
+// quotes around the url).
+//
+// You'll have to authorize the script to act with your account (yes, this
+// should feel scary), there are two permissions the script needs, to run
+// whon you're not present.  (so it can respond to requests from Habitica
+// through the webhooks interface, and run time delayed triggers to wait
+// for things to happen), and to contact external websites.  You can check
 // that the only website this script contacts is habitica by searching
-// the script for http
-const WEB_APP_URL = 'problem';
+// the script for UrlFetchApp and noticing that only habiticaApi calls
+// that function and it prepends habitica's API url to every request.
+const WEB_APP_URL = 'replace_me';
 
 // ==========================================
 // [Users] Configuration                     
@@ -61,13 +65,15 @@ const NUM_DAYS_UNTIL_INACTIVE = 3.0;
 const NUM_HOURS_UNTIL_FORCE_START = 24.0;
 
 // SpellSpammer:
+// TODO change variable names to be generic.
 const FIREBALL_STRING = 'Smash Rage';
+// TODO create string for skill key.
 
 // ==========================================
 // [Users] Do not edit code below this line
 // ==========================================
 const AUTHOR_ID = '074e86a5-a37e-4f73-8826-461f7514ac70';
-const SCRIPT_NAME = 'Playground';
+const SCRIPT_NAME = 'UserMultiScript';
 const HEADERS = {
   'x-client' : AUTHOR_ID + '-' + SCRIPT_NAME,
   'x-api-user' : USER_ID,
